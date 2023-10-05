c44f27a7c478e4cf0c960cea9047d68a44733f7b
rajgautamr <rajgautamr0307@gmail.com>
2023-10-05 15:31:00 +0530

    improved contact section

--- src/components/contact us/Contact.jsx
+++ src/components/contact us/Contact.jsx
@@ -8,18 +8,18 @@ const Contact = () => {
         <div class="column3">
         
             <h2>Contact Us</h2>
-            <p>This is the content of the first column.</p>
+            
         </div>
         <div class="column2">
 
         <div class="form-container">
       <form class="form">
         <div class="form-group">
-          <label for="email">Company Email</label>
+          <label for="email">Your Email</label>
           <input type="text" id="email" name="email" required=""></input>
         </div>
         <div class="form-group">
-          <label for="textarea">How Can We Help You?</label>
+          <label for="textarea">Your Message Here</label>
           <textarea name="textarea" id="textarea" rows="10" cols="50" required="">          </textarea>
         </div>
         <button class="form-submit-btn" type="submit">Submit</button>
